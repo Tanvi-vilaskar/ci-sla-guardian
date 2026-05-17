434af864ac7476158a5b31bfeeee943616db5491
Tanvi-Vilaskar <tvilaskar@gmail.com>
2026-05-17 21:44:05 +0530

    loop1.cbl added for testing

--- sample-data/loop1.cbl
+++ sample-data/loop1.cbl
@@ -52,9 +52,3 @@
                    END-PERFORM
                END-PERFORM
            END-PERFORM.
-
-       UNUSED-LOGIC.
-           DISPLAY 'THIS IS DEAD CODE - NEVER EXECUTED'.
-           MOVE 999999 TO WS-I
-           MOVE 999999 TO WS-J
-           MOVE 999999 TO WS-K.
