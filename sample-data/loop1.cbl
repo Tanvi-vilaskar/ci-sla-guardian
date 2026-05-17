bdeecfd79a02af0e863e513b3747783201ad5d8d
Tanvi-Vilaskar <tvilaskar@gmail.com>
2026-05-18 04:59:02 +0530

    added new loop progran for new demo

--- sample-data/loop1.cbl
+++ sample-data/loop1.cbl
@@ -24,8 +24,10 @@
                        PERFORM VARYING M FROM 1 BY 1 UNTIL M > 25
                           PERFORM VARYING N FROM 1 BY 1 UNTIL N > 10
                              PERFORM VARYING O FROM 1 BY 1 UNTIL O > 5
+                              PERFORM VARYING O FROM 1 BY 1 UNTIL O > 3
                                    COMPUTE TEMP = I * J + K * L + M * N 
                                    ADD TEMP TO SUM
+                              END-PERFORM
                              END-PERFORM
                           END-PERFORM
                        END-PERFORM
