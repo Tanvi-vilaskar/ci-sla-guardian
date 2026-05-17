e54c2ee62ffeed3620613adbcbe7412313c19478
Tanvi-Vilaskar <tvilaskar@gmail.com>
2026-05-18 03:56:43 +0530

    new loop1.cbl removeone loop

--- sample-data/loop1.cbl
+++ sample-data/loop1.cbl
@@ -24,10 +24,8 @@
                        PERFORM VARYING M FROM 1 BY 1 UNTIL M > 25
                           PERFORM VARYING N FROM 1 BY 1 UNTIL N > 10
                              PERFORM VARYING O FROM 1 BY 1 UNTIL O > 5
-                               PERFORM VARYING P FROM 1 BY 1 UNTIL P > 3
                                  COMPUTE TEMP = I * J + K * L + M * N 
                                    ADD TEMP TO SUM
-                                END-PERFORM
                              END-PERFORM
                           END-PERFORM
                        END-PERFORM
