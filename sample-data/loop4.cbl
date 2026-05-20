1f0fcdedffce0d53aa05c6e67cf3302e4a5d680c
Tanvi-Vilaskar <tvilaskar@gmail.com>
2026-05-21 01:53:27 +0530

    without sla breach testing

---
+++ sample-data/loop4.cbl
@@ -0,0 +1,16 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. SMALLSAFE1.
+
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       01  I               PIC S9(9) COMP VALUE 0.
+       01  SUM1             PIC S9(18) COMP VALUE 0.
+       01  VALUE-TABLE.
+           05  VALUE-ITEM  PIC S9(9) COMP OCCURS 50 TIMES
+                            INDEXED BY IDX.
+
+       PROCEDURE DIVISION.
+
+       FINISH-PARA.
+           DISPLAY "SMALLSAFE1 SUM = " SUM1
+           STOP RUN.
