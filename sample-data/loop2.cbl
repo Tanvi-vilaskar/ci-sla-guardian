39cc2f87278009b8cc6ae0430f6cbed833589dba
Tanvi-Vilaskar <tvilaskar@gmail.com>
2026-05-25 01:05:05 +0530

    remove one loop from loop2

--- sample-data/loop2.cbl
+++ sample-data/loop2.cbl
@@ -18,13 +18,11 @@
            PERFORM VARYING I FROM 1 BY 1 UNTIL I > 200
               PERFORM VARYING J FROM 1 BY 1 UNTIL J > 150
                  PERFORM VARYING K FROM 1 BY 1 UNTIL K > 100
-                    PERFORM VARYING L FROM 1 BY 1 UNTIL L > 50
                              COMPUTE TEMP =
                                    I * J
                                  + K * L
                                  + M * N
                              ADD TEMP TO SUM1
-                    END-PERFORM
                  END-PERFORM
               END-PERFORM
            END-PERFORM
