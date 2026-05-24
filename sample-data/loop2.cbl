211b3dfad3ee694c671f5248e3cd29cd2ffe7e95
Tanvi-Vilaskar <tvilaskar@gmail.com>
2026-05-25 01:55:46 +0530

    small chnages

--- sample-data/loop2.cbl
+++ sample-data/loop2.cbl
@@ -19,11 +19,13 @@
               PERFORM VARYING J FROM 1 BY 1 UNTIL J > 150
                  PERFORM VARYING K FROM 1 BY 1 UNTIL K > 100
                      PERFORM VARYING K FROM 1 BY 1 UNTIL L > 200
+                        PERFORM VARYING K FROM 1 BY 1 UNTIL M > 200
                              COMPUTE TEMP =
                                    I * J
                                  + K * L
                                  + M * N
                              ADD TEMP TO SUM1
+                        END-PERFORM
                      END-PERFORM
                  END-PERFORM
               END-PERFORM
